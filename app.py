60f39c1b395bb7a349ff00cf94b6cb832be6417f
Paul Kenny <redpaulkenny@gmail.com>
2019-11-10 18:26:26 +0000

    Add basic display of listings to home page

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
-from flask import Flask, render_template, redirect, request, url_for, flash
+from flask import Flask, render_template, redirect, request, url_for, flash, jsonify
 from flask_pymongo import PyMongo
 from forms import ListingsForm
 import os
+import requests
+import requests_cache
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "cinema_db"
 app.config["MONGO_URI"] = os.getenv("MONGO_URI", "mongodb://localhost")
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "thisismysecretkey")
 
+
 mongo = PyMongo(app)
+
+requests_cache.install_cache(
+    'requests-cache', backend='mongodb', expire_after=85600, connection=mongo.cx)
+OMDB_KEY = os.getenv("OMDB_KEY")
 
 
 @app.route("/")
 @app.route("/index")
 def home():
-    return render_template("index.html", title="Listings")
+    raw_listings = mongo.db.listings.find({}, {'_id': 0, 'film_name': 1})
+    listings = []
+    for raw_listing in raw_listings:
+        listing = {}
+        listing['film_name'] = raw_listing['film_name']
+        url = f'http://www.omdbapi.com/?apikey={OMDB_KEY}&t={listing["film_name"]}'
+        r = requests.get(url).json()
+        listing['poster'] = r['Poster']
+        listings.append(listing)
+    return render_template("index.html", title="Listings", listings=listings)
 
 
 @app.route("/now-showing")
