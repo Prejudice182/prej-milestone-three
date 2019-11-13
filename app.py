0b08a1ec086d758ce564d7342f572045aa332b5f
Paul Kenny <redpaulkenny@gmail.com>
2019-11-13 02:28:04 +0000

    Display top 3 favourites as cards

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for, flash, jsonify
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, pymongo
 from forms import EntryForm
 import os
 import requests
@@ -21,9 +21,9 @@ OMDB_KEY = os.getenv('OMDB_KEY')
 @app.route('/')
 @app.route('/index')
 def home():
-    movies = mongo.db.movie.find() or None
-    tvshows = mongo.db.series.find() or None
-    return render_template('index.html', title='Home', movies=movies, tvshows=tvshows)
+    movies = mongo.db.movie.find()
+    tvshows = mongo.db.series.find()
+    return render_template('index.html', title='Home', movies=movies.sort('votes', pymongo.DESCENDING).limit(3), tvshows=tvshows.sort('votes', pymongo.DESCENDING).limit(3))
 
 
 @app.route('/add-favourite', methods=['GET', 'POST'])
@@ -33,21 +33,24 @@ def add_favourite():
         url = f'http://www.omdbapi.com/?apikey={OMDB_KEY}&t={request.form["entry_name"]}&type={request.form["entry_type"]}'
         entry_details = requests.get(url).json()
         try:
-            coll_name = mongo.db[request.form["entry_type"]]
+            coll_name = mongo.db[request.form['entry_type']]
             coll_name.insert_one({
-                "title": entry_details["Title"],
-                "year": entry_details["Year"],
-                "release": entry_details["Released"],
-                "genres": entry_details["Genre"],
-                "plot": entry_details["Plot"],
-                "poster": entry_details["Poster"],
-                "imdbRating": entry_details["imdbRating"],
-                "reason": request.form["reason"],
-                "addedBy": request.form["username"]
+                'title': entry_details['Title'],
+                'year': entry_details['Year'],
+                'release': entry_details['Released'],
+                'genres': entry_details['Genre'],
+                'plot': entry_details['Plot'],
+                'poster': entry_details['Poster'],
+                'imdbRating': entry_details['imdbRating'],
+                'reason': request.form['reason'],
+                'addedBy': request.form['username'],
+                'votes': 1
             })
-        except Exception as e:
-            flash(f'{e}')
-        return redirect(url_for("home"))
+        except:
+            flash('Something went wrong! Please try again')
+        else:
+            flash("Favourite saved!")
+            return redirect(url_for('home'))
     return render_template('add-favourite.html', title='Add Favourite', form=entry_form)
 
 
--- forms.py
+++ forms.py
@@ -1,12 +1,53 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, SubmitField, validators
 from wtforms.validators import InputRequired
+from wtforms.widgets import Select, html_params, HTMLString
+
+# Code taken from https://stackoverflow.com/questions/23460857/create-selectfield-options-with-custom-attributes-in-wtforms
+
+
+class AttribSelect(Select):
+    """
+    Renders a select field that supports options including additional html params.
+
+    The field must provide an `iter_choices()` method which the widget will
+    call on rendering; this method must yield tuples of
+    `(value, label, selected, html_attribs)`.
+    """
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault('id', field.id)
+        if self.multiple:
+            kwargs['multiple'] = True
+        html = ['<select %s>' % html_params(name=field.name, **kwargs)]
+        for val, label, selected, html_attribs in field.iter_choices():
+            html.append(self.render_option(
+                val, label, selected, **html_attribs))
+        html.append('</select>')
+        return HTMLString(''.join(html))
+
+
+class AttribSelectField(SelectField):
+    widget = AttribSelect()
+
+    def iter_choices(self):
+        for value, label, render_args in self.choices:
+            yield (value, label, self.coerce(value) == self.data, render_args)
+
+    def pre_validate(self, form):
+        if self.choices:
+            for v, _, _ in self.choices:
+                if self.data == v:
+                    break
+            else:
+                raise ValueError(self.gettext('Is Not a valid choice'))
+# End code from Stack Overflow
 
 
 class EntryForm(FlaskForm):
     entry_name = StringField('Movie / TV Show Name', [InputRequired()])
-    entry_type = SelectField('Type', [InputRequired()], choices=[
-                            ('', 'Please select a type'), ('movie', 'Movie'), ('series', 'TV Show')])
+    entry_type = AttribSelectField('Type', [InputRequired()], choices=[
+        ('', 'Please select a type', dict(disabled='disabled')), ('movie', 'Movie', dict()), ('series', 'TV Show', dict())], default='')
     username = StringField('Your Name', [InputRequired()])
     reason = TextAreaField('Reason', [InputRequired()])
     submit = SubmitField('Post Listing')
