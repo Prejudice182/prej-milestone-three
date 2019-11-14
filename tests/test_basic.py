9a3abe38223e95070f4799516a105c0b577b64fc
Paul Kenny <redpaulkenny@gmail.com>
2019-11-14 20:00:17 +0000

    Add some basic unittests

---
+++ tests/test_basic.py
@@ -0,0 +1,50 @@
+import os
+import unittest
+
+from application import create_app, mongo
+
+
+class BasicTests(unittest.TestCase):
+    def setUp(self):
+        app = create_app()
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['MONGO_URI'] = 'mongodb+srv://prejudice182:Oiheivai123@myfirstcluster-1ow6a.mongodb.net/tests?retryWrites=true&w=majority'
+        mongo.init_app(app)
+        self.app = app.test_client()
+        mongo.db.movie.drop()
+        mongo.db.series.drop()
+
+    def tearDown(self):
+        pass
+
+    def addFav(self, entry, eType, name, reason):
+        return self.app.post('/add-favourite', data=dict(
+            entry_name=entry,
+            entry_type=eType,
+            username=name,
+            reason=reason
+        ), follow_redirects=True)
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_add_favourite(self):
+        response = self.addFav('Great Escape', 'movie',
+                               'Prejudice', 'Blash blah')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Favourite saved!', response.data)
+
+    def test_invalid_add_favourite(self):
+        response = self.addFav('The Great Escape', 'movie',
+                               'Prejudice', 'Blash blah')
+        self.assertEqual(response.status_code, 200)
+        response = self.addFav('The Great Escape', 'movie',
+                               'Prejudice', 'Blash blah')
+        self.assertIn(b'Someone already added that one!', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
