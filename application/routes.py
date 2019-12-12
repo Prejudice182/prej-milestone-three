400694a24c40d3307f12d083181634332a59944a
Paul Kenny <redpaulkenny@gmail.com>
2019-12-12 19:00:30 +0000

    Add favicons and logo

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
 # Import required modules
-from flask import Blueprint, render_template, abort, request
+from flask import Blueprint, render_template, abort, request, send_from_directory
+import os
 from . import mongo
 from flask_pymongo import pymongo
 from flask import current_app as app
@@ -38,7 +39,8 @@ def view_all(entry_type):
     # Get skip argument from URL
     if request.args.get('skip') is not None:
         # Change skip from str to int if its a number, else 0
-        skip = int(request.args.get('skip')) if request.args.get('skip').isnumeric() else 0
+        skip = int(request.args.get('skip')) if request.args.get(
+            'skip').isnumeric() else 0
         # Check if skip has been set to negative by malicious actor
         skip = 0 if skip < 0 else skip
     else:
@@ -46,10 +48,16 @@ def view_all(entry_type):
 
     # Retrieve entries from database
     entries_count = favs.count_documents({'Type': db_type}, skip=skip)
-    entries = favs.find({'Type': db_type}).sort('Votes', pymongo.DESCENDING).skip(skip).limit(6)
+    entries = favs.find({'Type': db_type}).sort(
+        'Votes', pymongo.DESCENDING).skip(skip).limit(6)
     return render_template('view-all.html', title=title, entries=entries, entry_type=entry_type, entries_count=entries_count, skip=skip)
 
 # Error handler for entire app if page not found
 @main_bp.app_errorhandler(404)
 def page_not_found(error):
     return render_template('404.html', title='404'), 404
+
+
+@main_bp.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static/images'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
